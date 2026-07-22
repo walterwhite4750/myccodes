7ad6c494c93799188eb08d0cbe78138e0053a946
VIVEGAN <walterwhite4750@gmail.com>
2026-07-22 13:22:17 +0530

    array min

---
+++ array_min.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{int arr[5] = {1,2,3,4,5};
+     int min = arr[0];
+     for(int i=0;i<5;i++){
+         if(arr[i]<min){
+             min = arr[i];
+         }
+     }
+     printf("minimum value of the array is:%d",min);
+    return 0;
+}
